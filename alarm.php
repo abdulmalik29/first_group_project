a603013093654dbacfcb66e9ae0400a0aa8aca88
d42339bs <ben.smith-7@student.manchester.ac.uk>
2020-01-31 11:48:30 +0000

    Alarm page

---
+++ alarm.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="myStyles.css">
+		<title>Alarm - Homies</title>
+		<meta charset="UTF-8"> <!-- Set the character set -->
+	</head>
+	<body>
+		<div class="column sidenav">
+			<h1>Homies</h1>
+			<ul>
+				<li><a>Home</a></li>
+				<li><a>Alarm</a></li>
+				<li><a>Chat</a></li>
+				<li><a>Shopping</a></li>
+				<li><a>Finance</a></li>
+				<li><a>Trash</a></li>
+				<li><a>Complaints</a></li>
+				<li><a>Members</a></li>
+			</ul>
+		</div>
+		<div class="header row">
+			<h2>Alarm</h2>
+		</div>
+		<div class="row">
+			<div class="column content">
+				<div class="row">
+					<div class="column alarm-section">
+						<h2>Who is in:</h2>
+						    <ul>
+								<li><a>Ben</a></li>
+
+							</ul>
+						</h2>
+					</div>
+					<div class="column alarm-section">
+						<h2>Who is out:</h2>
+						    <ul>
+								<li><a>Rahul</a></li>
+
+							</ul>
+						</h2>
+					</div>
+				</div>
+				<div class="row alarm-user-status">
+                   <h3>Are you in?</h3>
+                   <a>Yes </a></br>
+                   <a>No </a>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="footer">
+			<p>First Year Team Project 2020 by Ben Smith</p>
+		</div>
+	</body>
+</html>
