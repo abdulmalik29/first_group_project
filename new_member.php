d441cc9377efded3c1e3d9e93ad22d848cd6c64d
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-01-31 11:47:20 +0000

    new_member_1

---
+++ new_member.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NEW MEMBER REGISTER</title>
+</head>
+
+<body>
+
+	<img src="homies.png" alt="Homies logo"><br>
+	<h1>NEW MEMBER REGISTER</h1>
+  
+	<form action="" method="post">
+		
+  		<div class="container">
+        <label for="invitecode"><b>Invite Code</b></label>
+        <input type="text" placeholder="Enter invite code" name="invite code" required>
+        <br>
+
+    		<label for="housename"><b>House Name</b></label>
+    		<input type="text" placeholder="Enter House Name" name="housename" required>
+   			<br>
+
+   			<label for="username"><b>Username</b></label>
+    		<input type="text" placeholder="Enter Username" name="username" required>
+        <br>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+        <br>
+
+        <label for="eml"><b>Email</b></label>
+        <input type="email" placeholder="Enter Email" name="eml" required>
+        <br>
+
+        <label for="phone"><b>Phone number</b></label>
+        <input type="text" placeholder="Enter Phone number" name="pnumber" required>
+
+    		<br><br>
+    		<button type="submit">Create</button>
+
+    		<br>
+    		<label>
+      			<input type="checkbox" checked="checked" name="remember"> Remember me
+    		</label>
+  		</div>
+	</form>
+
+</body>
+</html>
