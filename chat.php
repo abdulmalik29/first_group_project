eb11805514fa3ea7a70a39eea440e57afe6a5e43
Abdul-malik Al-Ghazi <abdul-malik.al-ghazi@student.manchester.ac.uk>
2020-01-31 11:52:01 +0000

    chat

---
+++ chat.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+<style>
+.side a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #ffffff;
+  display: block; }
+
+.side {
+  height: 100%;
+  width: 170px;
+  position: fixed;
+  top: 100;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+text.fixed {
+  position: fixed;
+  bottom: 400;
+  right: 400;
+  width: 300px;
+  border: 3px solid #73AD21
+</style>
+
+
+</head>
+<body>
+
+<div id="text" class="text">
+<input type="text">
+</div>
+
+<h1>Chat</h1>
+<h2>Topics</h2>
+<div id="side" class="side">
+  <a href="#">Christmas</a>
+  <a href="#">Hiking</a>
+  <a href="#">Video games</a>
+  <a href="#">Memes</a>
+</div>
