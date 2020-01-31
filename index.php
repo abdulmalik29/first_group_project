aed1d90ff8706dac97d3a3e32851f3a4bcfd8136
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-01-31 11:45:51 +0000

    index_1

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>INDEX</title>
+</head>
+<body>
+	<h1>HOMIES</h1>
+	<img src="homies.png" alt="Homies Logo"><br>
+	<a href="login.php">Login</a>
+	<a href="new_house.php">New House</a>
+	<a href="new_member.php">New Member</a>
+
+	<h2>ABOUT</h2>
+	<p>Homies is an online system that makes sharing a house easier! Forget the problems of who owes who what, do I need to set the alarm? Do we need milk or not? All of these issues can now be kept track of and stored in one place. If you're at the shop and dont know if you need milk or not, just log into Homies and voilà! Yes you do. Homies helps you to keep track of all of these simple things that are usually spread over many group chats and hidden in random messages.</p>
+
+	<h3>Functions:</h3>
+	<h4>ALARM</h4>
+	<p>The alarm function allows house members to let other members of the house know whether or not they are present inside the house. This allows them to find out whether or not they have to set an alarm or not.</p>
+
+	<h4>Shopping List</h4>
+	<p>This funtion allows you as the user to let everyone know when something is out in your house. If you are out of butter but you are busy for the next couple of hours or know someone else is heading to the shop then you can just add it into the shopping list for the next person who is at the shop to log in and check.</p>
+
+	<h4>Finances</h4>
+	<p>The shopping list also takes care of who owes who what. If one friend has made a specific request then you can keep track of your finances </p>
+
+	<h4>Complaints</h4>
+	<p>Here you and all the other members of your house will be able to lodge any complaints etc via an online form that will be stored in the database and be sent to the landlord. Allowing you to easily keep track of when and what was complained about.</p>
+	
+	<h4>Chat</h4>
+	<p>The chat is very straightforward, it is a central chat for the house that will allow users to talk about anything. It can serve as a location to only discuss house related things or as a mainy group chat for memes and the like!</p>
+
+</body>
+</html>
