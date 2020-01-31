7a63d8dd4446426a0b6926905294baed4864f74b
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-01-31 11:46:29 +0000

    login_1

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+</head>
+
+<body>
+	<img src="homies.png" alt="Homies logo"><br>
+	<h1>MEMBER LOGIN</h1>
+	<form action="" method="post">
+		
+  		<div class="container">
+    		<label for="uname"><b>Username</b></label>
+    		<input type="text" placeholder="Enter Username" name="uname" required>
+   			<br>
+
+   			 <label for="psw"><b>Password</b></label>
+    		<input type="password" placeholder="Enter Password" name="psw" required>
+
+    		<br><br>
+    		<button type="submit">Login</button>
+
+    		<br>
+    		<label>
+      			<input type="checkbox" checked="checked" name="remember"> Remember me
+    		</label>
+  		</div>
+
+  		
+  		<div class="container" style="background-color:#f1f1f1">
+    		<span class="psw"><a href="#">Forgot password?</a></span>
+  		</div>
+	</form>
+</body>
+</html>
