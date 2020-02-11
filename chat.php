7caff920adab00924e59ca45156b1d8b432ac932
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-02-11 23:10:51 +0000

    Unified design

--- chat.php
+++ chat.php
@@ -1,48 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-
-
-<style>
-.side a {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #ffffff;
-  display: block; }
-
-.side {
-  height: 100%;
-  width: 170px;
-  position: fixed;
-  top: 100;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-text.fixed {
-  position: fixed;
-  bottom: 400;
-  right: 400;
-  width: 300px;
-  border: 3px solid #73AD21
-</style>
-
-
-</head>
+	<head>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Chat</title>
+	</head>
 <body>
-
-<div id="text" class="text">
-<input type="text">
-</div>
-
-<h1>Chat</h1>
-<h2>Topics</h2>
-<div id="side" class="side">
-  <a href="#">Christmas</a>
-  <a href="#">Hiking</a>
-  <a href="#">Video games</a>
-  <a href="#">Memes</a>
-</div>
+	<div class="leftcol">
+		<h1 align=center>HOMIES</h1>
+		<a href="alarm.php" id="menulinks">Alarm</a><br>
+		<a href="chat.php" id="menulinks">Chat</a><br>
+		<a href="shopping.php" id="menulinks">Finance and Shopping</a><br>
+		<a href="trash.php" id="menulinks">Trash</a><br>
+		<a href="complaints.php" id="menulinks">Complaints</a><br>
+		<a href="members.php" id="menulinks">Members</a><br>
+	</div>
+	<div class="rightcol">
+		<h1>Chat</h1>
+		<h2>Topics</h2>
+	<div class="leftcol">
+		<a href="#">Christmas</a><br>
+		<a href="#">Hiking</a><br>
+		<a href="#">Video games</a><br>
+		<a href="#">Memes</a>
+	</div >
+</body>
+</html>
