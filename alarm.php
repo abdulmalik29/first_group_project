30a563ae25e610d440362391cf1eee303682a744
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-02-11 23:09:58 +0000

    Unified design

--- alarm.php
+++ alarm.php
@@ -1,57 +1,29 @@
+<!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="myStyles.css">
-		<title>Alarm - Homies</title>
-		<meta charset="UTF-8"> <!-- Set the character set -->
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Alarm</title>
 	</head>
 	<body>
-		<div class="column sidenav">
-			<h1>Homies</h1>
-			<ul>
-				<li><a>Home</a></li>
-				<li><a>Alarm</a></li>
-				<li><a>Chat</a></li>
-				<li><a>Shopping</a></li>
-				<li><a>Finance</a></li>
-				<li><a>Trash</a></li>
-				<li><a>Complaints</a></li>
-				<li><a>Members</a></li>
-			</ul>
+		<div class="leftcol">
+			<h1 align=center>HOMIES</h1>
+			<a href="alarm.php" id="menulinks">Alarm</a><br>
+			<a href="chat.php" id="menulinks">Chat</a><br>
+			<a href="shopping.php" id="menulinks">Finance and Shopping</a><br>
+			<a href="trash.php" id="menulinks">Trash</a><br>
+			<a href="complaints.php" id="menulinks">Complaints</a><br>
+			<a href="members.php" id="menulinks">Members</a><br>
 		</div>
-		<div class="header row">
-			<h2>Alarm</h2>
-		</div>
-		<div class="row">
-			<div class="column content">
-				<div class="row">
-					<div class="column alarm-section">
-						<h2>Who is in:</h2>
-						    <ul>
-								<li><a>Ben</a></li>
-
-							</ul>
-						</h2>
-					</div>
-					<div class="column alarm-section">
-						<h2>Who is out:</h2>
-						    <ul>
-								<li><a>Rahul</a></li>
-
-							</ul>
-						</h2>
-					</div>
-				</div>
-				<div class="row alarm-user-status">
-                   <h3>Are you in?</h3>
-                   <a>Yes </a></br>
-                   <a>No </a>
-
-				</div>
-			</div>
-		</div>
-
-		<div class="footer">
-			<p>First Year Team Project 2020 by Ben Smith</p>
+		<div class="rightcol">
+			<h1>Alarm</h1>
+			<h2>Who is in:</h2>
+			<a>Ben</a>
+			<h2>Who is out:</h2>
+			<a>Rahul</a>
+            <h3>Are you in?</h3>
+            <a>Yes </a></br>
+            <a>No </a>
 		</div>
 	</body>
 </html>
