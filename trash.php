5ca120b032b46f3a0826bff6740d1dc08c222c48
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-02-11 23:12:53 +0000

    Unified design

---
+++ trash.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Trash and Recycling</title>
+</head>
+<body>
+	<div class="leftcol">
+		<h1 align=center>HOMIES</h1>
+		<a href="alarm.php" id="menulinks">Alarm</a><br>
+		<a href="chat.php" id="menulinks">Chat</a><br>
+		<a href="shopping.php" id="menulinks">Finance and Shopping</a><br>
+		<a href="trash.php" id="menulinks">Trash</a><br>
+		<a href="complaints.php" id="menulinks">Complaints</a><br>
+		<a href="members.php" id="menulinks">Members</a><br>
+	</div>
+	<div class="rightcol">
+		<h1>Trash</h1>
+		<p>We have to do this guys!</p>
+	</div>
+</body>
+</html>
