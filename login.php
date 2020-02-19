76b8250ae8dff6060182c779c94a2fd1e4b849fa
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-02-19 15:19:04 +0000

    x

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     		<input type="text" placeholder="Enter Username" name="uname" required>
    			<br>
 
-   			 <label for="psw"><b>Password</b></label>
+   			<label for="psw"><b>Password</b></label>
     		<input type="password" placeholder="Enter Password" name="psw" required>
 
     		<br><br>
