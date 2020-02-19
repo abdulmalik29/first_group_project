5420464a701330b2f9438d92104e2444bf78d0a5
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-02-19 15:52:29 +0000

    test

--- login_process.php
+++ login_process.php
@@ -9,24 +9,8 @@ $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbna
 if($mysqli -> connect_error) {
     die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
 }
-else{
-    session_start();
-    if ( ! empty( $_POST ) ) {
-        if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
-            // Getting submitted user data from database
-            $con = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
-            $stmt = $con->prepare("SELECT * FROM users WHERE username = ?");
-            $stmt->bind_param('s', $_POST['username']);
-            $stmt->execute();
-            $result = $stmt->get_result();
-        	$user = $result->fetch_object();
-        		
-        	// Verify user password and set $_SESSION
-        	if ( password_verify( $_POST['password'], $user->password ) ) {
-        		$_SESSION['user_id'] = $user->ID;
-        	}
-        }
-    }
-}
+
+echo 'Hello ' . htmlspecialchars($_POST["uname"]) . '!';
+
 $mysqli -> close();
 ?>
