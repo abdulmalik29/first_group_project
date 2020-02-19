1ac44d7bf913dddcce25ddd53f6b2e2f578a01d7
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-02-19 15:24:34 +0000

    test

--- login_process.php
+++ login_process.php
@@ -9,34 +9,22 @@ $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbna
 if($mysqli -> connect_error) {
     die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
 }
-else {
-    if($_SERVER["REQUEST_METHOD"] == "POST")
-      {
-       // username and password sent from form 
-         $myusername=mysqli_real_escape_string($group_dbnames[0],$_POST['uname']); 
-         $mypassword=mysqli_real_escape_string($group_dbnames[0],$_POST['psw']); 
-         echo($myusername + $mypassword);
-     
-         $sql="SELECT username FROM User WHERE username='$myusername' and password='$mypassword'";
-         $result=mysqli_query($db,$sql);
-         $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-         $active=$row['active'];
-     
-         $count=mysqli_num_rows($result);
-     
-        // If result matched $myusername and $mypassword, table row must be 1 row
-        if($count==1)
-        {
-         session_register("myusername");
-         $_SESSION['login_user']=$myusername;
-     
-         header("location: members.php");
-        }
-        else 
-        {
-        $error="Your Login Name or Password is invalid";
-        }
-      }
+
+if ( ! empty( $_POST ) ) {
+    if ( isset( $_POST['uname'] ) && isset( $_POST['psw'] ) ) {
+        // Getting submitted user data from database
+        $con = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+        $stmt = $con->prepare("SELECT * FROM User WHERE username = ?");
+        $stmt->bind_param('s', $_POST['uname']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    	$user = $result->fetch_object();
+    		
+    	// Verify user password and set $_SESSION
+    	if ( password_verify( $_POST['psw'], $user->password ) ) {
+    		$_SESSION['username'] = $user->ID;
+    	}
+    }
 }
 
 $mysqli -> close();
