a7671af139ab081ae10c781be760f6bcf2f915f5
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-03-04 16:52:53 +0000

    change back

--- newhouse_process.php
+++ newhouse_process.php
@@ -21,12 +21,12 @@ if($mysqli -> connect_error) {
     
     $password = password_hash($psw, PASSWORD_DEFAULT);
     
-    $sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
-    //$sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
-    if($mysqli->query($sql2)){
-        $sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
-        //$sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
-        if($mysqli->query($sql1)){
+    //$sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
+    $sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
+    if($mysqli->query($sql1)){
+        //$sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
+        $sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
+        if($mysqli->query($sql2)){
             $_SESSION['username'] = $uname;
             $_SESSION['houseID'] = $row["houseID"];
             header("Location: alarm.php");
