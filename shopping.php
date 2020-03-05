c3ea1569347dec6e8006ba5b12e0721917bf55a6
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-05 18:40:11 +0000

    V1

--- shopping.php
+++ shopping.php
@@ -32,32 +32,7 @@ else {
 		<a href="members.php" id="menulinks">Members</a><br>
 	</div>
 	<div class="rightcol">
-	    <?php
-	        require_once('config.inc.php');
-            $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
-             // Check for errors before doing anything else
-            if($mysqli -> connect_error) {
-                die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
-            }
-            return $mysqli;
-            
-            #$Date = mysqli_real_escape_string($mysqli, $_POST['dateReported']);
-            #mysqli_real_escape_string($_POST['shoppigID']), 
-            $name = mysqli_real_escape_string($mysqli, $_POST['buyerName']);
-            $itemBought = mysqli_real_escape_string($mysqli, $_POST['item']);
-            $itemPrice = mysqli_real_escape_string($mysqli, $_POST['price']); 
-            #mysqli_real_escape_string($_POST['houseID'])
-            
-            $sql = "INSERT INTO Shopping (shoppingID, buyerName, item, price, houseID) VALUES (1, $name, $itemBought, $itemPrice, 1)";
-            
-            #$insert = $mysqli->query[$sql];
-            
-            if(mysqli_query($mysqli, $sql)){
-                    echo "Records added successfully.";
-            } else{
-                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
-            }
-        ?>
+	    
 		<table>
 			<h1 align="center" width=100%>Shopping List</h1>
 			<tr>
