a511d1e2fe7e89547ce9b23c163d4fdc4aeae775
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-05 10:43:13 +0000

    Some progress, error ln44

--- shopping.php
+++ shopping.php
@@ -33,34 +33,27 @@ else {
 		<a href="members.php" id="menulinks">Members</a><br>
 	</div>
 	<div class="rightcol">
+	    <?php
+	        require_once('config.inc.php');
+            $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+             // Check for errors before doing anything else
+            if($mysqli -> connect_error) {
+                die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+            }
+            return $mysqli;
+             $sql = "INSERT INTO 'Shopping' (shoppingID, buyerName, item, price, houseID) VALUES ('{mysqli_real_escape_string($_POST['shoppigID'])}', '{mysqli_real_escape_string($_POST['buyerName'])}', '{mysqli_real_escape_string($_POST['item'])}', '{mysqli_real_escape_string($_POST['price'])}', '{mysqli_real_escape_string($_POST['houseID'])}' )";
 		<table>
 			<h1 align="center" width=100%>Shopping List</h1>
 			<tr>
-				<td>
-					<?php echo $_GET["item1"]?>
-					<?php echo $_GET["user1"]?>
-					<?php echo $_GET["price1"]?>
-				</td>
-				<td>
-					<?php echo $_GET["item2"]?>
-					<?php echo $_GET["user2"]?>
-					<?php echo $_GET["price2"]?>
-				</td>
 			</tr>
 			<tr>
 				<td>
-					<form action="shopping.php" method="get">
-						<input type="text" name="item1">
-						<input type="text" name="user1">
-						<input type="text" name="price1">
-						<input type="submit" value="Submit">
-					</form>
-				</td>
-				<td>
-					<form action="shopping.php" method="get">
-						<input type="text" name="item2">
-						<input type="text" name="user2">
-						<input type="text" name="price2">
+					<form action="" method="post">
+						<input type="text" name="shoppingID">
+						<input type="text" name="buyerName">
+						<input type="text" name="item">
+						<input type="text" name="price">
+						<input type="text" name="houseID">
 						<input type="submit" value="Submit">
 					</form>
 				</td>
