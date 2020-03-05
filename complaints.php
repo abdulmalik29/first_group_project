d13d15dcb7bcf04f67998f128bc28b682aa84e2e
Rahul Sanku <rahul.sanku@student.manchester.ac.uk>
2020-03-05 19:58:18 +0000

    test

--- complaints.php
+++ complaints.php
@@ -57,17 +57,6 @@ else {
 </body>
 </html>
 <?php
-    function setupConnection() {
-        require_once('config.inc.php');
-        $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
-
-         // Check for errors before doing anything else
-        if($mysqli -> connect_error) {
-            die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
-        }
-        return $mysqli;
-    }
-
     $currentUsername = $_SESSION['username'];
     
     
@@ -91,9 +80,6 @@ else {
     // Close connection
     mysqli_close($mysqli);
     
-    function closeConnection($mysqli) {
-    // Always close your connection to the database cleanly!
-    $mysqli -> close();
     }
 
 ?>
