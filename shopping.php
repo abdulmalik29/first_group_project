7ca55d1a9e1afc5261e07cf76b21b41becdf02b3
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-05 18:55:46 +0000

    V6

--- shopping.php
+++ shopping.php
@@ -38,34 +38,41 @@ else {
 			</tr>
 			<tr>
 				<td>
-					<form action="" method="post">
-					    <label>Name</label>
-						<input type="text" name="buyerName"><br>
-						<label>Item</label>
-						<input type="text" name="item"><br>
-						<label>Price</label>
-						<input type="text" name="price"><br>
-						<input type="submit" value="Submit">
-					</form>
-					 <?php
-                        $currentHouseID = $_SESSION['houseID'];
-                        #$Date = mysqli_real_escape_string($mysqli, $_POST['dateReported']);
-                        #mysqli_real_escape_string($_POST['shoppigID']), 
-                        $name = $mysqli->real_escape_string($mysqli, $_POST['buyerName']);
-                        $itemBought = $mysqli->real_escape_string($mysqli, $_POST['item']);
-                        $itemPrice = $mysqli->real_escape_string($mysqli, $_POST['price']); 
-                        #mysqli_real_escape_string($_POST['houseID'])
-                        
-                        $sql = "INSERT INTO Shopping (shoppingID, buyerName, item, price, houseID) VALUES (1, $name, $itemBought, $itemPrice, $currentHouseID)";
-                        
-                        if(mysqli_query($mysqli, $sql)){
-                                echo "Records added successfully.";
-                        } else{
-                                echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
-                        }
-                    ?>
+					<?php
+					    displayForm($mysqli);
+					    processUserInput($mysqli);
+					?>
 				</td>
 			</tr>
 	  </table>
 </body>
 </html>
+<?php
+    function processUserInput($mysqli){
+        $currentHouseID = $_SESSION['houseID'];
+        #$Date = mysqli_real_escape_string($mysqli, $_POST['dateReported']);
+        #mysqli_real_escape_string($_POST['shoppigID']), 
+        $name = $mysqli->real_escape_string($mysqli, $_POST['buyerName']);
+        $itemBought = $mysqli->real_escape_string($mysqli, $_POST['item']);
+        $itemPrice = $mysqli->real_escape_string($mysqli, $_POST['price']); 
+        #mysqli_real_escape_string($_POST['houseID'])
+        $sql = "INSERT INTO Shopping (shoppingID, buyerName, item, price, houseID) VALUES (1, $name, $itemBought, $itemPrice, $currentHouseID)";
+        if(mysqli_query($mysqli, $sql)){
+            echo "Records added successfully.";
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+        }
+    }
+    function displayForm($mysqli) {
+        echo    '<form action="" method="post">
+			        <label>Name</label>
+					<input type="text" name="buyerName"><br>
+				    <label>Item</label>
+					<input type="text" name="item"><br>
+					<label>Price</label>
+					<input type="text" name="price"><br>
+					<input type="submit" value="Submit">
+				</form>';
+    }
+
+?>
