2621eaed60753c341df245c7880a69856ce0da34
Rahul Sanku <rahul.sanku@student.manchester.ac.uk>
2020-03-06 11:55:09 +0000

    test

---
+++ inputcomplaints.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+
+    if ( isset( $_SESSION['username'] ) && isset( $_SESSION['houseID'] ) ) {
+        // Grab user data from the database using the user_id
+        // Let them access the "logged in only" pages
+        require_once('config.inc.php');
+        $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+    
+        // Check for errors before doing anything else
+        if($mysqli -> connect_error) {
+    	die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+        }
+    }
+    else {
+        // Redirect them to the login page
+        header("Location: login.php");
+        $_SESSION['access_attempted'] = true;
+        exit;
+    }
+
+    
+    $currentHouseID = $_SESSION['houseID'];
+    $currentUsername = $_SESSION['username'];
+    $Date = mysqli_real_escape_string($mysqli, $_POST['dateReported']);
+    $Date = mysqli_real_escape_string($mysqli, $_POST['dateReported']);
+    $Location = mysqli_real_escape_string($mysqli, $_POST['Location']);
+    $Issue = mysqli_real_escape_string($mysqli, $_POST['complaint']);
+    $sql = "SELECT MAX(complaintID) FROM Complaints";
+    $ComplaintID = mysqli_query($sql, $mysqli);
+    $ComplaintID = $ComplaintID + 1;
+    $sql = "INSERT INTO `Complaints` (`complaintID`, `username`, `complaint`, `Location`, `sorted`, 'dateReported') VALUES ('1121', 'testman', 'lol', 'lol', '0', '2020-03-13')";
+    if(mysqli_query($mysqli, $sql)){
+        echo "Records added successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+    }
