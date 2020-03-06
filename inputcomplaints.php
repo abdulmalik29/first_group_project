4303925e6151c45e9cb5bf12ff7136058424ae3d
Rahul Sanku <rahul.sanku@student.manchester.ac.uk>
2020-03-06 22:15:03 +0000

    test

--- inputcomplaints.php
+++ inputcomplaints.php
@@ -28,7 +28,7 @@
     $sql1 = "SELECT MAX(complaintID) FROM Complaints";
     $ComplaintID = mysqli_query($mysqli, $sql1);
     echo $ComplaintID;
-    $sql = "INSERT INTO Complaints (complaintID, username, complaint, Location, sorted, dateReported) VALUES ('$ComplaintID', '$currentUsername', '$Issue', '$Location', '0', '$Date')";
+    $sql = "INSERT INTO Complaints (username, complaint, Location, sorted, dateReported) VALUES ('$currentUsername', '$Issue', '$Location', '0', '$Date')";
     if($mysqli->query($sql)) {       
         echo "Records added successfully.";
     } else{
