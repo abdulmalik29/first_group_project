f7147e9c636b3138c63b3995bc2085c678056c53
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-06 10:06:24 +0000

    fixed?

--- shopping.php
+++ shopping.php
@@ -8,6 +8,13 @@ if ( isset( $_SESSION['username'] ) && isset( $_SESSION['houseID'] ) ) {
     // Grab user data from the database using the user_id
     // Let them access the "logged in only" pages
     //$mysqli = setupConnection();
+    require_once('config.inc.php');
+    $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
+    // Check for errors before doing anything else
+    if($mysqli -> connect_error) {
+        die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+    }
 }
 else {
     // Redirect them to the login page
