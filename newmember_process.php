8c209a94383721184fe7ec046a42b5b873e1f024
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-03-08 15:57:36 +0000

    newmember test

---
+++ newmember_process.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+// Load the configuration file containing your database credentials
+require_once('config.inc.php');
+
+
+// Connect to the database
+$mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
+// Check for errors before doing anything else
+if($mysqli -> connect_error) {
+    die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+}
+    $hname = ($_POST["housename"]);
+    $names = ($_POST["fname"]);
+    $uname = ($_POST["username"]);
+    $psw = ($_POST["psw"]);
+    $email = ($_POST["eml"]);
+    $phone_number = ($_POST["pnumber"]);
+    
+    $password = password_hash($psw, PASSWORD_DEFAULT);
+    
+    //$sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
+    $sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
+    if($mysqli->query($sql1)){
+        //$sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
+        $sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
+        if($mysqli->query($sql2)){
+            $_SESSION['username'] = $uname;
+            $_SESSION['houseID'] = $row["houseID"];
+            header("Location: alarm.php");
+        }
+        else{
+            echo($mysqli->error);
+            //header("Location: new_house.php"); 
+        }
+    }
+    else{
+        echo($mysqli->error);
+        //header("Location: new_house.php"); 
+        // must have failed to create house
+    }
+    
+
+    $mysqli -> close();
+
+?>
