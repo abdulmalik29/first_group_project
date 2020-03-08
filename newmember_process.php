0a191c2163a478ed8252dfea861f6c2cc63f03ae
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-03-08 16:09:57 +0000

    new user test

--- newmember_process.php
+++ newmember_process.php
@@ -12,37 +12,41 @@ $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbna
 if($mysqli -> connect_error) {
     die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
 }
+    $hid = ($_POST["houseid"]);
     $hname = ($_POST["housename"]);
-    $names = ($_POST["fname"]);
+    $fullname = ($_POST["fullname"]);
     $uname = ($_POST["username"]);
     $psw = ($_POST["psw"]);
     $email = ($_POST["eml"]);
     $phone_number = ($_POST["pnumber"]);
     
     $password = password_hash($psw, PASSWORD_DEFAULT);
-    
-    //$sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
-    $sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
+
+    $sql1 = "SELECT houseID, housename FROM House";
+    $result = $mysqli->query($sql1);
     if($mysqli->query($sql1)){
-        //$sql1 = "INSERT INTO House (housename, masterusername) VALUES ('$hname', '$uname')";
-        $sql2 = "INSERT INTO User (username, password, email, phonenumber, name, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$names', '0')";
-        if($mysqli->query($sql2)){
-            $_SESSION['username'] = $uname;
-            $_SESSION['houseID'] = $row["houseID"];
-            header("Location: alarm.php");
-        }
-        else{
-            echo($mysqli->error);
-            //header("Location: new_house.php"); 
+        while($row = $result->fetch_assoc()) {
+            if($row["houseID"] == $hid){
+                if($row["housename"] == $hname){
+                    $sql2 = "INSERT INTO User (username, password, email, phonenumber, name, houseID, outside) VALUES ('$uname', '$password', '$email', '$phone_number', '$fullname', '$hid', '0')";
+                    if($mysqli->query($sql2)){
+                        $_SESSION['username'] = $uname;
+                        $_SESSION['houseID'] = $row["houseID"];
+                        header("Location: alarm.php");
+                        }
+                    else{
+                        echo($mysqli->error);
+                        header("Location: new_member.php"); 
+                    }
+                }
+            }
         }
     }
     else{
         echo($mysqli->error);
-        //header("Location: new_house.php"); 
-        // must have failed to create house
+        header("Location: new_member.php"); 
     }
     
-
     $mysqli -> close();
 
 ?>
