a4db82d1488f37cbc3cb657fe70f09a5850483f1
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-03-10 15:32:51 +0000

    in/out

--- alarm.php
+++ alarm.php
@@ -26,7 +26,7 @@ else {
 	<body>
 		<div class="leftcol">
 			<h1 align=center>HOMIES</h1>
-			<a href="alarm.php" id="menulinks">Alarm</a><br>
+			<a href="alarm.php" id="menulinks">In/Out</a><br>
 			<a href="shopping.php" id="menulinks">Finance and Shopping</a><br>
 			<a href="trash.php" id="menulinks">Trash</a><br>
 			<a href="complaints.php" id="menulinks">Complaints</a><br>
@@ -34,7 +34,7 @@ else {
             <a href="logout.php" id='menulinks'>Logout</a><br>
 		</div>
 		<div class="rightcol">
-		    <h1>Alarm</h1>
+		    <h1>In/Out</h1>
 		    <?php 
 		    getPeople($mysqli); 
 		    ?>
