f8314ed8d9106e02ab57e474db22dde2438815a7
Rahul Sanku <rahul.sanku@student.manchester.ac.uk>
2020-03-10 15:38:28 +0000

    method to add landlord emails into the database

---
+++ inputlandlord.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    if ( isset( $_SESSION['username'] ) && isset( $_SESSION['houseID'] ) ) {
+        // Grab user data from the database using the user_id
+        // Let them access the "logged in only" pages
+        require_once('config.inc.php');
+        $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+    
+        // Check for errors before doing anything else
+        if($mysqli -> connect_error) {
+    	die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+        }
+    }
+    else {
+        // Redirect them to the login page
+        header("Location: login.php");
+        $_SESSION['access_attempted'] = true;
+        exit;
+    }
+
+    
+    $currentHouseID = $_SESSION['houseID'];
+    $currentUsername = $_SESSION['username'];
+    $owneremail = mysqli_real_escape_string($mysqli, $_POST['owneremail']);
+    $sql = "UPDATE Complaints SET ownerEmail = " . $owneremail . " WHERE houseID = " . $currentHouseID;
+    if($mysqli->query($sql)) {       
+        echo "<script>
+             alert('Email added successfully'); 
+             window.history.go(-1);
+     </script>";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+    }
