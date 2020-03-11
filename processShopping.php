e6ab538c30bb9644a548459b2c33e36c4a078252
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-11 12:44:20 +0000

    shared, lets see

--- processShopping.php
+++ processShopping.php
@@ -25,14 +25,15 @@
     $i_price = mysqli_real_escape_string($mysqli, $_POST['item_price']);
     $o_name = mysqli_real_escape_string($mysqli, $_POST['owner_name']);
     
-    $sql = "INSERT INTO Shopping (buyerName, item, price, houseID) VALUES ('$b_name', '$i_name', '$i_price', '$currentHouseID')";
-
-    if($mysqli->query($sql)) {       
+    $sql1 = "INSERT INTO Shopping (buyerName, item, price, houseID) VALUES ('$b_name', '$i_name', '$i_price', '$currentHouseID')";
+    $sql2 = "INSERT INTO ShoppingSharedTo (username, houseID) VALUES ('$o_name', '$currentHouseID')";
+    
+    if($mysqli->query($sql1) && $mysqli->query($sql2)) {       
         echo "<script>
              alert('Records added successfully'); 
              window.history.go(-1);
      </script>";
     } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+        echo "ERROR: Could not able to execute $sql1 or $sql2 " . mysqli_error($mysqli);
     }
 ?>
