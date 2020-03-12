b39fff2591b1168a605da9f4e1251c2659afa435
Mathias Belayneh <mathias.belayneh@student.manchester.ac.uk>
2020-03-12 23:34:58 +0000

    l

--- alarm.php
+++ alarm.php
@@ -107,11 +107,11 @@ function showAlarmForm($mysqli) {
 	echo '<form action="set_alarm.php" method="post">';
 	
 	if ($row['outside'] === "0") {
-	    echo '<button type="submit" class="alarm" id="yes" name="go-outside">Leave house</button>';
+	    echo '<p align="center"><button type="submit" class="alarm" id="yes" name="go-outside">Leave house</button></p>';
 	    
 	}
 	else {
-	    echo '<button type="submit" class="alarm" id="no" name="go-inside">Enter house</button>';
+	    echo '<p align="center"><button type="submit" class="alarm" id="no" name="go-inside">Enter house</button></p>';
 	}
 	echo '</form>';
     }
