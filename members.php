cfdf7559806acf11d87fa9e12dbe3a850b446aa1
Rahul Sanku <rahul.sanku@student.manchester.ac.uk>
2020-03-16 17:07:11 +0000

    test

--- members.php
+++ members.php
@@ -51,7 +51,7 @@ else {
     				    	<input type='submit' value='Submit'>
     		    </form>";
             } else {
-                echo  "Owner Email: ";
+                echo  "Owner Email: " . $result1;
             }
 		?>
 
