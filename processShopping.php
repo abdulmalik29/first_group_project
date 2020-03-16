8c40a883d3515071fd02836b8cf661648762b5c2
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-16 15:41:07 +0000

    ??

--- processShopping.php
+++ processShopping.php
@@ -31,7 +31,7 @@
         }else{
             $query1="SELECT TOP 1 shoppingID FROM Shopping ORDER BY shoppingID DESC";
             $records1 = $mysqli->query($query1);
-            $next_index = $records1['shoppingID']+ 1;
+            $next_index = (int) $records1['shoppingID']+1;
         }
         $i_name =  mysqli_real_escape_string($mysqli, $_POST['item_name_b']);
         $o_name = mysqli_real_escape_string($mysqli, $_POST['owner_name_b']);
