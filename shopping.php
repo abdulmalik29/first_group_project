5c028f532d34788f5fd81f18d4a4227a884088a4
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-18 12:31:43 +0000

    brr

--- shopping.php
+++ shopping.php
@@ -60,7 +60,7 @@ function displayItems($mysqli){
         $items3="SELECT requesterName, item FROM Request WHERE houseID = " . $currentHouseID . " AND requestID IN (SELECT requestID FROM RequestSharedTo WHERE username = '" . $b_name . "')";
         $itemRecords3 = $mysqli->query($items3);
         
-        echo '<div width=70% style="float: left; border: 2px solid black">
+        echo '<div width=55% style="float: left; border: 2px solid black">
             <div class="scrollable" style="float: left"><table align="left" width="100%" cellspacing="5" cellpadding="8">
             <tr><th colspan="2" align="center">You Bought</th></tr><tr>
             <td align="left">Item</td>
@@ -73,7 +73,7 @@ function displayItems($mysqli){
                 $row['item'] . '</td><td align="left">' .
                 $row['price'] . '</td></tr>';
         }
-        echo '</table></div><br>';
+        echo '</table></div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
         
         echo '<div class="scrollable" style="float: left"><table align="left" width="100%" cellspacing="5" cellpadding="8">
             <tr><th colspan="3" align="center">Your Debts</th></tr><tr>
