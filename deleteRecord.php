4469708150b1ae733d74a2327c97f1c0ad576edf
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-18 13:57:30 +0000

    first

---
+++ deleteRecord.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    if ( isset( $_SESSION['username'] ) && isset( $_SESSION['houseID'] ) ) {
+        // Grab user data from the database using the user_id
+        // Let them access the "logged in only" pages
+        require_once('config.inc.php');
+        $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+    
+        // Check for errors before doing anything else
+        if($mysqli -> connect_error) {
+    	die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+        }
+    }
+    else {
+        // Redirect them to the login page
+        header("Location: login.php");
+        $_SESSION['access_attempted'] = true;
+        exit;
+    }
+    
+    $currentHouseID = $_SESSION['houseID'];
+    $u_name = $_SESSION['username'];
+  
+?>
