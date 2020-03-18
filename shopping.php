ca9698693f97d8c9dcfea4110c67948e6003684e
Peter Foris <peter.foris@student.manchester.ac.uk>
2020-03-18 11:40:12 +0000

    d

--- shopping.php
+++ shopping.php
@@ -75,7 +75,7 @@ function displayItems($mysqli){
         echo '</table></div>';
         
         echo '<div class="scrollable" style="float: left"><table align="left" width="100%" cellspacing="5" cellpadding="8">
-            <tr><th colspan="3" align="center">Bought for you</th></tr><tr>
+            <tr><th colspan="3" align="center">Your debts</th></tr><tr>
             <td align="left">Buyer</td>
             <td align="left">Item</td>
             <td align="left">Price</td>
