b7ea85fb5c9a93043a9fe08cefad3fdbc2581272
Frederick Hall <frederick.hall-2@student.manchester.ac.uk>
2020-03-22 15:56:46 +0000

    one less break

--- new_member.php
+++ new_member.php
@@ -94,7 +94,7 @@ h1{
   <button class="nav" onclick="window.location.href = 'index.php';">Home</button>
   <button class="nav" onclick="window.location.href = 'login.php';">Login</button>
 	<button class="nav" onclick="window.location.href = 'new_house.php';">New House</button>
-	<button class="nav" onclick="window.location.href = 'new_member.php';">New Member</button><br><br><br><br><br>
+	<button class="nav" onclick="window.location.href = 'new_member.php';">New Member</button><br><br><br><br>
 	<img src="homies.png" alt="Homies logo"><br>
   
 	<form action="newmember_process.php" method="post">
