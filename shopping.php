1296a262192f53acc6c89741aa167e55ee6f1f0c
Ben Smith <ben.smith-7@student.manchester.ac.uk>
2020-03-23 01:00:47 +0000

    test 2

--- shopping.php
+++ shopping.php
@@ -53,8 +53,9 @@ function displayItems($mysqli){
         
         //$items1="SELECT shoppingID, buyerName, item, price FROM Shopping WHERE houseID = " . $currentHouseID . " AND buyerName = " . $b_name;
         $stmt1 = $mysqli->prepare("SELECT shoppingID, buyerName, item, price FROM Shopping WHERE houseID = ? AND buyerName = ?");
-        echo $stmt1;
         $stmt1->bind_param("ss", $currentHouseID, $b_name);
+                echo $stmt1;
+
         //$itemRecords1 = $mysqli->query($items1);
         $itemRecords1 = $stmt1->execute();
         
